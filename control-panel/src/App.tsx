9e2c6ef2b35bb0b68c17d3e99aa8f75c0510dfaf
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-12-30 20:27:01 +0330

    uninstall shdcn

--- control-panel/src/App.tsx
+++ control-panel/src/App.tsx
@@ -1,5 +1,12 @@
+import Sidebar from './components/Sidebar';
+
 function App() {
-	return <div></div>;
+	return (
+		<div className="grid grid-cols-[256px_1fr]">
+			<header className="col-span-2"></header>
+			<Sidebar />
+		</div>
+	);
 }
 
 export default App;
