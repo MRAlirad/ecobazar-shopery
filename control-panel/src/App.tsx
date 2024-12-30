9a011aace43a8c449819465bb5d5fc92d48720c7
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-12-30 11:53:51 +0330

    install and config dependencies

--- control-panel/src/App.tsx
+++ control-panel/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-	return <div></div>;
+	return <div>afdsaf</div>;
 }
 
 export default App;
--- control-panel/src/main.tsx
+++ control-panel/src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import App from './App.tsx';
+import './index.css';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/"
+						element={<App />}
+					/>
+				</Routes>
+			</BrowserRouter>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</StrictMode>
 );
