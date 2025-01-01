947a569c40bf01147c3467916b0382174c2a701b
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-01-01 12:27:34 +0330

    button component => size, props

--- control-panel/src/components/Button.tsx
+++ control-panel/src/components/Button.tsx
@@ -1,21 +1,44 @@
+import { ReactNode } from 'react';
+import Loader from './Loader';
+import Icon from './Icon';
 import classnames from 'classnames';
 
 interface Props {
 	color?: 'default' | 'blue' | 'green' | 'red' | 'blue-outline' | 'green-outline' | 'red-outline';
-	type?: 'submit' | 'button';
 	text?: string;
+	icon?: ReactNode;
+	size?: 'small' | 'medium' | 'large';
+	type?: 'submit' | 'button';
+	fluid?: boolean;
+	disabled?: boolean;
+	loading?: boolean;
+	className?: string;
+	onClick?: () => void;
 }
 
-const Button = ({ color = 'blue', type = 'submit', text = '' }: Props) => {
+const Button = ({ color = 'blue', text = '', icon, size = 'medium', fluid = false, disabled = false, loading = false, type = 'submit', className = '', onClick = () => {} }: Props) => {
 	return (
 		<button
 			className={classnames({
 				btn: true,
 				[color]: true,
+				[size]: true,
+				disabled: disabled || loading,
+				'w-full': fluid,
+				[className]: className,
 			})}
+			disabled={disabled || loading}
+			onClick={onClick}
 			type={type}
 		>
-			{text}
+			{loading ? (
+				<Loader size="20" />
+			) : (
+				<>
+					{icon && <Icon>{icon}</Icon>}
+					{text && <span>{text}</span>}
+				</>
+			)}
 		</button>
 	);
 };
