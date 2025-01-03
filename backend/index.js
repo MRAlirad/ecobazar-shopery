a218e4306ecf57fac3193a5a9e6dee8b2d98068a
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-01-04 00:56:54 +0330

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,21 @@ const mongoose = require('mongoose');
 const express = require('express');
 const products = require('./routes/products');
 const customers = require('./routes/customers');
+const cors = require('cors');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/shopery')
+mongoose
+	.connect('mongodb://localhost/shopery')
 	.then(() => console.log('Connected to MongoDB...'))
-	.catch(err => console.error('Could not connect to MongoDB...'))
-;
+	.catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
+app.use(
+	cors({
+		origin: ['http://localhost:5173'],
+	})
+);
 app.use('/api/products', products);
 app.use('/api/customers', customers);
 
