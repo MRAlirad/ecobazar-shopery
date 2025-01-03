31e9b82482be3837981357943c8fd16b8f1cd35b
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-01-04 00:56:07 +0330

    Update apiClient.ts

--- control-panel/src/hooks/api/apiClient.ts
+++ control-panel/src/hooks/api/apiClient.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const apiClient = axios.create({
 	baseURL: 'http://localhost:8000/api',
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+	},
 });
 
 export default apiClient;
