1b4664c9b78d68de0b3b53ba7e2dd72799364adb
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-01-04 00:55:22 +0330

    Create values.ts

---
+++ control-panel/src/values.ts
@@ -0,0 +1,14 @@
+export const statuses: { value: number; label: string }[] = [
+	{
+		value: 1,
+		label: 'Active',
+	},
+	{
+		value: 2,
+		label: 'Deactive',
+	},
+	{
+		value: 3,
+		label: 'Archived',
+	},
+];
