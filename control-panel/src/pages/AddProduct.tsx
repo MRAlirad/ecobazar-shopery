beb1596d0a883a42a58899bbe76f6a9707e1d854
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-01-04 00:55:48 +0330

    Update AddProduct.tsx

--- control-panel/src/pages/AddProduct.tsx
+++ control-panel/src/pages/AddProduct.tsx
@@ -1,10 +1,17 @@
 import ProductForm from '../components/product/ProductForm';
+import { useAddProduct } from '../hooks/api';
 
 const AddProduct = () => {
+	const addProduct = useAddProduct({});
+
 	return (
 		<main className="page">
 			<h1>Add Product</h1>
-			<ProductForm />
+			<ProductForm
+				mode="ADD"
+				onAdd={data => addProduct.mutate(data)}
+				isAdding={addProduct.isPending}
+			/>
 		</main>
 	);
 };
