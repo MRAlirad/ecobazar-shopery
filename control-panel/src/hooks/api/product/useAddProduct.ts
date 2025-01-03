26554a856e8a654a66a60724929d92f1188025e7
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-01-04 00:56:31 +0330

    Update useAddProduct.ts

--- control-panel/src/hooks/api/product/useAddProduct.ts
+++ control-panel/src/hooks/api/product/useAddProduct.ts
@@ -1,10 +1,31 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import apiClient from "../apiClient";
-import apiConfig from "../config";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import apiClient from '../apiClient';
+import apiConfig from '../apiConfig';
+import ProductSchema from '../../../schemas/ProductSchema';
+import { toast } from 'react-toastify';
 
-const useAddProduct = ()=> {
-    const {path, queryKey} = apiConfig.product;
-    return useMutation({
-        mutationFn: (data: any) => apiClient.post(path, data)
-    })
+interface Props {
+	onAdd?: () => void;
+	onError?: () => void;
+	successToast?: string;
+}
+
+const useAddProduct = ({ onAdd = () => {}, onError = () => {} }: Props) => {
+	const { path, queryKey } = apiConfig.product;
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: (data: ProductSchema) => apiClient.post(path, data).then(res => res.data),
+		onSuccess: (data: ProductSchema) => {
+			console.log(data);
+			queryClient.invalidateQueries({ queryKey: [queryKey] });
+			onAdd();
+		},
+		onError: (error: Error) => {
+			toast.error(error.message);
+			onError();
+		},
+	});
 };
+
+export default useAddProduct;
