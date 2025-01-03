5286d3aa42096007e568b30a48c35f52a00bc278
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-01-04 00:56:35 +0330

    Update App.tsx

--- control-panel/src/App.tsx
+++ control-panel/src/App.tsx
@@ -1,64 +1,18 @@
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import { Outlet } from 'react-router';
-// import Select from './components/Select';
-// import { FormProvider, useForm } from 'react-hook-form';
-// import Button from './components/Button';
-// import Input from './components/Input';
-// import { yupResolver } from '@hookform/resolvers/yup';
-// import * as yup from 'yup';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
-	// const formMethods = useForm({
-	// 	resolver: yupResolver(
-	// 		yup.object().shape({
-	// 			country: yup.string().required(),
-	// 			email: yup.string().email().required(),
-	// 		})
-	// 	),
-	// 	defaultValues: {
-	// 		country: '1',
-	// 		email: 'test@gmail.com',
-	// 	},
-	// });
 	return (
 		<div className="grid grid-cols-[256px_1fr] grid-rows-[max-content_1fr] bg-white dark:bg-gray-900 min-h-screen">
 			<Header />
 			<Sidebar />
 			<Outlet />
-			{/* <FormProvider {...formMethods}>
-				<form
-					className="p-4 space-y-4"
-					onSubmit={formMethods.handleSubmit(data => console.log(data))}
-				>
-					<Select
-						label="shetopskey"
-						name="country"
-						options={[
-							{
-								value: 1,
-								label: 'United States',
-							},
-							{
-								value: 2,
-								label: 'Iran',
-							},
-							{
-								value: 3,
-								label: 'United Kingdom',
-							},
-						]}
-					/>
-					<Input
-						label="email"
-						name="email"
-					/>
-					<Button
-						text="submit"
-						type="submit"
-					/>
-				</form>
-			</FormProvider> */}
+			<ToastContainer
+				position="bottom-center"
+				theme="colored"
+			/>
 		</div>
 	);
 }
