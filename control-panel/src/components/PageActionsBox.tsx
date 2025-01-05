0f4335d4a9e77f2673e62e38e2b5fd4e6111df5e
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-01-05 13:12:14 +0330

    control-panel => page action box component

---
+++ control-panel/src/components/PageActionsBox.tsx
@@ -0,0 +1,44 @@
+import Button from './Button.tsx';
+
+interface Props {
+	mode: 'ADD' | 'EDIT';
+	onAdd?: () => void;
+	isAdding?: boolean;
+	onEdit?: () => void;
+	isEditing?: boolean;
+	onDelete?: () => void;
+	isDeleting?: boolean;
+}
+
+const PageActionsBox = ({ mode, onAdd = () => {}, isAdding, onEdit = () => {}, isEditing, onDelete = () => {}, isDeleting }: Props) => {
+	return (
+		<div className="actions-box">
+			{mode === 'ADD' && (
+				<Button
+					color="blue"
+					text="Save"
+					type="submit"
+					loading={isAdding}
+					onClick={onAdd}
+				/>
+			)}
+			{mode === 'EDIT' && (
+				<Button
+					color="green"
+					text="Edit"
+					type="submit"
+					loading={isEditing}
+					onClick={onEdit}
+				/>
+			)}
+			<Button
+				color="red-outline"
+				text="Delete"
+				loading={isDeleting}
+				onClick={onDelete}
+			/>
+		</div>
+	);
+};
+
+export default PageActionsBox;
