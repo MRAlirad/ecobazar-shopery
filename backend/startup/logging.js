2c31a8cd2eab786247c0ffaf35d3cb976c9f84ff
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2025-01-12 15:44:53 +0330

    backend => startup => logging

---
+++ backend/startup/logging.js
@@ -0,0 +1,11 @@
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function () {
+	winston.exceptions.handle(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+	winston.rejections.handle(new winston.transports.File({ filename: 'unhandledRejections.log' }));
+
+	winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+	winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost/shopery' }));
+};
