fa19402f347ce33b46a671ad308bd844c773796b
Mohammadreza <m.r.alirad75@gmail.com>
2025-04-27 12:27:11 +0330

    update UI Badge Component

--- control-panel/src/components/Badge.tsx
+++ control-panel/src/components/Badge.tsx
@@ -1,21 +1,19 @@
 import classNames from 'classnames';
 interface Props {
-	color: 'green' | 'red' | 'gray';
+	color?: 'green' | 'blue' | 'gray' | 'red' | 'yellow';
 	text: string;
+	className?: string;
+	size?: 'small' | 'medium' | 'large';
 }
 
-const Badge = ({ color, text }: Props) => {
-	const colorClass = {
-		green: 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300',
-		red: 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300',
-		gray: 'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300',
-	};
-
+const Badge = ({ text, color = 'blue', size = 'medium', className = '' }: Props) => {
 	return (
 		<span
 			className={classNames({
-				'text-sm font-medium px-2.5 py-0.5 rounded-full': true,
-				[colorClass[color]]: true,
+				badge: true,
+				[color]: true,
+				[size]: true,
+				[className]: className,
 			})}
 		>
 			{text}
