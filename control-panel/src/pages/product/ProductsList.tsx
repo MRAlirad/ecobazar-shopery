132ef5e0d2cca82e295d21bd55367f87587904c4
Mohammadreza <m.r.alirad75@gmail.com>
2025-05-16 15:50:56 +0330

    cp => refactor products table

--- control-panel/src/pages/product/ProductsList.tsx
+++ control-panel/src/pages/product/ProductsList.tsx
@@ -1,23 +1,28 @@
 import { useState } from 'react';
 import { useSearchParams } from 'react-router';
+import { useFirstMountState } from 'react-use';
 import { useGetProductsList, useDeleteProduct } from '../../hooks/api';
-import { TableListSkeleton } from '../../components/Skeletons';
+import { ListSkeleton } from '../../components/Skeletons';
 import Page from '../../components/Page';
 import PageHeader from '../../components/PageHeader';
-import Button from '../../components/Button';
 import Badge from '../../components/Badge';
 import { statuses } from '../../values';
 import DeleteModal from '../../components/DeleteModal';
-import Pagination from '../../components/Pagination';
-import ProductSchema from '../../schemas/ProductSchema';
-import { FaTrash, FaPen } from 'react-icons/fa';
+import { FaTrash, FaPen, FaShoppingCart } from 'react-icons/fa';
+import Table from '../../components/Table';
 
 const ProductsList = () => {
-	const [searchParams, setSearchParams] = useSearchParams();
+	const [searchParams] = useSearchParams();
+	const [deleteModalDisplay, setDeleteModalDisplay] = useState<null | string>(null);
+	const isFirstMount = useFirstMountState();
 
-	const { data: products, isLoading } = useGetProductsList({ params: searchParams.toString() });
+	const { data: products, isLoading: isProductLoading } = useGetProductsList({ params: searchParams.toString() });
 
-	if (isLoading) return <TableListSkeleton />;
+	const deleteProduct = useDeleteProduct({
+		successToast: 'Product deleted successfully',
+	});
+
+	if (isFirstMount && isProductLoading) return <ListSkeleton />;
 
 	return (
 		<Page type="list">
@@ -26,105 +31,88 @@ const ProductsList = () => {
 				action={{ text: 'Add Product', to: '/product/add' }}
 			/>
 
-			<div className="table-wrapper">
-				<table>
-					<thead>
-						<tr>
-							<th className="row">Row</th>
-							<th>Product name</th>
-							<th>Price</th>
-							<th>Discount</th>
-							<th>Status</th>
-							<th className="action">Actions</th>
-						</tr>
-					</thead>
-					<tbody>
-						{products?.data?.map((product, index) => (
-							<RowItem
-								key={product._id}
-								row={index + 1}
-								{...product}
-							/>
-						))}
-					</tbody>
-				</table>
-			</div>
+			<Table
+				columns={[
+					{ name: 'title', label: 'Product name' },
+					{ name: 'price', label: 'Price' },
+					{ name: 'discount', label: 'Discount' },
+					{ name: 'status', label: 'Status' },
+					{ name: 'action', label: '' },
+				]}
+				rows={(() => {
+					const rows = [];
 
-			{products && products?.totalPages > 1 && (
-				<Pagination
-					currentPage={products?.currentPage}
-					totalPages={products?.totalPages}
-					onChangePage={(page: number) => {
-						const params: Record<string, string> = {};
-
-						for (const [key, value] of searchParams.entries()) {
-							params[key] = value;
+					if (products)
+						for (const { _id, title, images, price, discount, status } of products.data) {
+							rows.push([
+								{
+									name: 'title',
+									component: (
+										<div className="flex items-center gap-2">
+											<div className="img-box size-14 aspect-square rounded">
+												<img
+													src={images[0]}
+													alt={title}
+													className="object-cover"
+												/>
+											</div>
+											<span className="font-bold">{title}</span>
+										</div>
+									),
+								},
+								{ name: 'price', value: price },
+								{ name: 'discount', value: `% ${discount}` },
+								{
+									name: 'status',
+									component: (
+										<Badge
+											color={statuses.find(s => s.value === status)?.badge ?? 'red'}
+											text={statuses.find(s => s.value === status)?.label ?? 'not defined'}
+										/>
+									),
+								},
+								{
+									name: 'action',
+									actions: [
+										{
+											text: 'edit',
+											icon: <FaPen size={16} />,
+											className: 'hover:!bg-gray-100 !text-gray-500',
+											to: `/product/${_id}`,
+										},
+										{
+											text: 'delete',
+											icon: <FaTrash size={16} />,
+											className: 'hover:!bg-red-100 !text-red-500',
+											loading: deleteProduct.isPending,
+											onClick: () => setDeleteModalDisplay(_id),
+										},
+									],
+									component: deleteModalDisplay === _id && (
+										<DeleteModal
+											title={`Are you sure you want to delete the "${title}" product?`}
+											onDelete={() => deleteProduct.mutate(_id)}
+											onClose={() => setDeleteModalDisplay(null)}
+											isDeleting={deleteProduct.isPending}
+										/>
+									),
+								},
+								{
+									name: 'link',
+									link: `/product/${_id}`,
+								},
+							]);
 						}
-						setSearchParams({ ...params, page: page.toString() });
-					}}
-				/>
-			)}
+
+					return rows;
+				})()}
+				isLoading={isProductLoading}
+				pagination={{
+					currentPage: +searchParams.get('page') || 1,
+					totalPages: products?.totalPages || 1,
+				}}
+			/>
 		</Page>
-	);
-};
-
-const RowItem = ({ row, _id, title, status, price, discount, images }: ProductSchema & { row: number }) => {
-	const [deleteModalDisplay, setDeleteModalDisplay] = useState(false);
-	const deleteProduct = useDeleteProduct({
-		successToast: 'Product deleted successfully',
-	});
-
-	return (
-		<>
-			<tr key={_id}>
-				<td>{row}</td>
-				<td>
-					<div className="flex items-center gap-2">
-						<div className="img-box size-14 aspect-square rounded">
-							<img
-								src={images[0]}
-								alt={title}
-								className="object-cover"
-							/>
-						</div>
-						<span className="font-bold">{title}</span>
-					</div>
-				</td>
-				<td>{price}</td>
-				<td>%{discount}</td>
-				<td>
-					<Badge
-						color={status === 1 ? 'green' : status === 2 ? 'red' : 'gray'}
-						text={statuses.find(s => s.value === status)?.label ?? ''}
-					/>
-				</td>
-				<td>
-					<div className="flex items-center gap-2">
-						<Button
-							color="green"
-							size="icon"
-							icon={<FaPen size="15" />}
-							to={`/product/${_id}`}
-						/>
-						<Button
-							color="red"
-							size="icon"
-							icon={<FaTrash size="15" />}
-							loading={deleteProduct.isPending}
-							onClick={() => setDeleteModalDisplay(true)}
-						/>
-					</div>
-				</td>
-			</tr>
-			{deleteModalDisplay && (
-				<DeleteModal
-					title={`Are you sure you want to delete the "${title}" product?`}
-					onClose={() => setDeleteModalDisplay(false)}
-					onDelete={() => _id && deleteProduct.mutate(_id)}
-					isDeleting={deleteProduct.isPending}
-				/>
-			)}
-		</>
 	);
 };
 
